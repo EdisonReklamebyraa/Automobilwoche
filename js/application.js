eb55038465bad0f60de47ff1d4064389923abf4d
Thomas Flogstad <thomas@edison.no>
2011-06-20 11:50:58 +0200

    implementing tabs

--- js/application.js
+++ js/application.js
@@ -316,6 +316,17 @@ $('.tickeranim').vTicker({
    direction: 'up'
 });
 
-
+$(function () {
+    var tabContainers = $('div.tabs > div');
+    
+    $('div.tabs ul.tabNavigation a').click(function () {
+        tabContainers.hide().filter(this.hash).show();
+        
+        $('div.tabs ul.tabNavigation a').removeClass('selected');
+        $(this).addClass('selected');
+        
+        return false;
+    }).filter(':first').click();
+});
 
 });
