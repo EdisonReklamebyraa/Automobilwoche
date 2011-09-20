10bbbcadbc64e417186f430d81126c751d87378c
mmichelli <mario@edison.no>
2011-09-20 16:17:23 +0200

    update email link

--- js/application.js
+++ js/application.js
@@ -399,8 +399,8 @@ $(function()
 function addFancyboxToPage(){
 
   $(".addthis .email").fancybox({
-    'width'       : "50%",
-    'height'      : 500,
+    'width'       : 600,
+    'height'      : 450,
     'autoScale'       : false,
     'transitionIn'    : 'none',
     'transitionOut'   : 'none',
